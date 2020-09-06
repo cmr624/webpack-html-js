f2a23c0a24009fc931c472428aa5d5cf400b2f42
Carlos Michael Rodriguez <cmr624@nyu.edu>
2020-09-06 16:25:24 -0400

    adding note

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+// if we want to copy more HTML
+// https://stackoverflow.com/questions/32155154/webpack-config-how-to-just-copy-the-index-html-to-the-dist-folder
 module.exports = {
   mode : "development",
   devtool:"inline-source-map",
