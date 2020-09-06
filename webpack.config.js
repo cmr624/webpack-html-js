af8ed32ec0b79272b9a07c94d3fd071f2e2b6cea
Carlos Michael Rodriguez <cmr624@nyu.edu>
2020-09-06 16:23:27 -0400

    removing unnecessary

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   mode : "development",
   devtool:"inline-source-map",
@@ -26,18 +25,12 @@ module.exports = {
       },
     ],
   },
-  
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
   plugins:[
-    new CopyPlugin({
-      patterns: [
-        { from: './src', to: './dist' },
-      ],
-    }),
     new HTMLWebpackPlugin({
       template:"./src/index.html"
     })
